d3870f44d60a53f2e0aec4abfce866f81132d6b7
Michelangelo <mickeycp2006@gmail.com>
2025-05-12 12:51:53 +0200

    aggiornamento 2025 05 12 12:51

--- codice/inseriscifilm.php
+++ codice/inseriscifilm.php
@@ -77,7 +77,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inserisci Film</title>
     <link rel="stylesheet" href="../css/style.css">
-    <script src="../js/popup.js"></script>
+    <script src="../js/Popup.js"></script>
 </head>
 <body>
     <h1>Inserisci un nuovo film</h1>
