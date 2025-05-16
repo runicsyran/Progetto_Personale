42052e345673b856dc4006c43a042f57960da335
Michelangelo <mickeycp2006@gmail.com>
2025-05-16 10:50:08 +0200

    aggiornamento 2025 05 16 10:50

--- codice/dettaglifilm.php
+++ codice/dettaglifilm.php
@@ -51,9 +51,11 @@ session_start();
         $user_id = $_SESSION['user_id'] ?? null;
         $user_role = $_SESSION['user_role'] ?? 'guest';
 
-        var_dump($_SESSION + "<br>");
-        var_dump($user_role + "<br>");
-        var_dump($user_id + "<br>");
+        var_dump($_SESSION);
+        
+        var_dump($user_role);
+        
+        var_dump($user_id);
         
         // Recupera l'ID del film dalla query string
 
