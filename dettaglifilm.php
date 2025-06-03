ac6573d1183a5aebd70b50c2b6e6eec94b3fbf0d
Michelangelo <mickeycp2006@gmail.com>
2025-06-03 10:39:12 +0200

    aggiornamento 2025 06 03 10:39

--- dettaglifilm.php
+++ dettaglifilm.php
@@ -79,7 +79,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_SESSION['user_id'])) {
     <div style="padding: 3vw;">
         <?php
         var_dump($_GET);
-        var_dump(value: $_SESSION);
+        var_dump($_SESSION);
         // Recupera l'ID del film dalla query string
         if (isset($_GET['id']) && is_numeric($_GET['id'])) {
             $film_id = intval($_GET['id']);
